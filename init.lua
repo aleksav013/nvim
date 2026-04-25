577af5230010c322e0bd5cc3ffcf8e6878659adb
Aleksa Vuckovic <aleksa@vuckovic.cc>
2026-04-25 10:12:38 +0200

    nvim 0.12

--- init.lua
+++ init.lua
@@ -1,10 +1,150 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.cmp"
-require "user.autopairs"
-require "user.lsp"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.colorscheme"
-require "user.autocommands"
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+vim.opt.signcolumn = "yes"
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣", }
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.hlsearch = true
+vim.opt.breakindent = true
+vim.opt.wrap = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.textwidth = 80
+
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+    },
+  },
+  virtual_text = true,
+})
+
+vim.pack.add({ "https://github.com/windwp/nvim-autopairs" }, { confirm = false })
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.pack.add({ "https://github.com/morhetz/gruvbox" }, { confirm = false })
+
+vim.cmd [[
+try
+       colorscheme gruvbox
+catch /^Vim\%((\a\+)\)\=:E185/
+       colorscheme default
+       set background=dark
+endtry
+]]
+
+vim.cmd "hi Normal guibg=NONE ctermbg=NONE"
+
+
+vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" }, { confirm = false })
+require("nvim-treesitter.install").update("all")
+vim.pack.add({ "https://github.com/saghen/blink.cmp" }, { confirm = false })
+
+require("blink.cmp").setup({
+  completion = {
+    documentation = {
+      auto_show = true,
+    },
+  },
+
+  keymap = {
+    ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+    ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+    ['<C-y>'] = { 'select_and_accept', 'fallback' },
+    ['<C-e>'] = { 'cancel', 'fallback' },
+    ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+
+    ['<Tab>'] = { 'snippet_forward', 'fallback' },
+    ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+    ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+    ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+    ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+  },
+
+  fuzzy = {
+    implementation = "lua",
+  },
+})
+
+local lsp_servers = {
+  lua_ls = {
+    Lua = { workspace = { library = vim.api.nvim_get_runtime_file("lua", true) }, },
+  },
+  clangd = {},
+  rust_analyzer = {},
+  bashls = {},
+}
+
+vim.pack.add({
+  "https://github.com/neovim/nvim-lspconfig",
+
+  "https://github.com/mason-org/mason.nvim",
+  "https://github.com/mason-org/mason-lspconfig.nvim",
+  "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim"
+}, { confirm = false })
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-tool-installer").setup({
+  ensure_installed = vim.tbl_keys(lsp_servers),
+})
+
+for server, config in pairs(lsp_servers) do
+  vim.lsp.config(server, {
+    settings = config,
+
+    on_attach = function(_, bufnr)
+      vim.keymap.set("n", "grd", vim.lsp.buf.definition,
+        { buffer = bufnr, desc = "vim.lsp.buf.definition()", })
+
+      vim.keymap.set("n", "grf", vim.lsp.buf.format,
+        { buffer = bufnr, desc = "vim.lsp.buf.format()", })
+    end,
+  })
+end
+
+
+vim.pack.add({
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/nvim-tree/nvim-web-devicons",
+  "https://github.com/nvim-telescope/telescope.nvim"
+}, { confirm = false })
+
+require("telescope").setup({})
+
+local pickers = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>sp", pickers.builtin, { desc = "[S]earch Builtin [P]ickers", })
+vim.keymap.set("n", "<leader>sb", pickers.buffers, { desc = "[S]earch [B]uffers", })
+vim.keymap.set("n", "<leader>sf", pickers.find_files, { desc = "[S]earch [F]iles", })
+vim.keymap.set("n", "<leader>sw", pickers.grep_string, { desc = "[S]earch Current [W]ord", })
+vim.keymap.set("n", "<leader>sg", pickers.live_grep, { desc = "[S]earch by [G]rep", })
+vim.keymap.set("n", "<leader>sr", pickers.resume, { desc = "[S]earch [R]esume", })
+
+vim.keymap.set("n", "<leader>sh", pickers.help_tags, { desc = "[S]earch [H]elp", })
+vim.keymap.set("n", "<leader>sm", pickers.man_pages, { desc = "[S]earch [M]anuals", })
+
+
+vim.pack.add({ "https://github.com/folke/which-key.nvim" }, { confirm = false })
+
+require("which-key").setup({
+  spec = {
+    { "<leader>s", group = "[S]earch", icon = { icon = "", color = "green", }, },
+  }
+})
--- lua/user/autocommands.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd [[
-]]
---  augroup _general_settings
---    autocmd!
---    autocmd BufWritePost *.tex silent !pdflatex %:p
---  augroup end
